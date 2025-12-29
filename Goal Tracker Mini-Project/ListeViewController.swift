ea3368949f8f07fad4e854cc8cc0b8c9f397e78e
Tufan Akbaş <tufanakbastr@gmail.com>
2025-12-29 23:13:26 +0300

    Harcama silme özelliği (Swipe to Delete) eklendi ve UITableView DataSource/Delegate yapısı açıklandı

--- Goal Tracker Mini-Project/ListeViewController.swift
+++ Goal Tracker Mini-Project/ListeViewController.swift
@@ -7,15 +7,49 @@
 
 import UIKit
 
-class ListeViewController: UIViewController {
+class ListeViewController: UIViewController, UITableViewDataSource,UITableViewDelegate {
+    
+    @IBOutlet weak var tableView: UITableView!
     
     var gelenHarcamalar: [Harcama] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        print("İkinci ekrana gelen harcama sayısı: \(gelenHarcamalar.count)")
+        tableView.dataSource = self
+        tableView.delegate = self
         
+        self.title = "Harcama Geçmişi"
+        
+    }
+        func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            return gelenHarcamalar.count
+        }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            
+            let cell = tableView.dequeueReusableCell(withIdentifier: "HarcamaCell", for: indexPath)
+            
+            let harcama = gelenHarcamalar[indexPath.row]
+            
+            cell.textLabel?.text = harcama.kategori
+            
+            cell.detailTextLabel?.text = "\(harcama.miktar) TL"
+            
+            return cell
+        }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        
+        
+        if editingStyle == .delete {
+            
+            gelenHarcamalar.remove(at: indexPath.row)
+            
+            tableView.deleteRows(at: [indexPath], with: .fade)
+            
+            print("Bir harcama silindi. Kalan: \(gelenHarcamalar.count)")
+        }
     }
     
 
